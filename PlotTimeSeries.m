cbd6aa7f89cad50b80506903cb8fe5bf46c5e81c
keith roberts <krober10@nd.edu>
2017-12-12 14:57:37 -0500

    Add files via upload

--- PlotTimeSeries.m
+++ PlotTimeSeries.m
@@ -1,26 +1,19 @@
 clear all; close all; clc;
 %plot time series from output of syncIMEDS python script
-noStat=193;
-load StationNames
+load NOAAStaExist.mat
+noStat=length(NOAAStaExist);
 jj=1; %counter for storing
 plotFig = 1;
 %read in location of stations
-locs = dlmread('SANDY_VAL.V2.INTERP6364.STATIONS.txt');
-locs = locs(2:end,:);
-OFFSET=0;
-
-for i = 1:noStat
+locs = dlmread('noaa.stations.forinterp6364');
+locs = locs(2:end,:); 
+for i =1:79
     file = ['sync_STATION_',num2str(i)];
     name = ['STATION ',num2str(i)];
     fid = fopen(file,'r');
     if fid < 0 %file doesn't exist
         continue
     else
-        %bad data
-        if i == 26 || i == 25
-            continue
-        end
-        
         
         data=textscan(fid,'%q %f %f','HeaderLines',1,'Delimiter',',');
         dmytime = data{1}; %time vector
@@ -39,63 +32,69 @@ for i = 1:noStat
         end
         obs(obs<-1000) = NaN; %dry value
         %obsLP=wlevel_filter_24h(datetime,obs,.0028,.3968); %low pass with a cutoff of 0.50 h.
-        if i==3 || i==4 %some problems with these two stations.
-            obs(obs<0.50)=NaN;
-            obsLP=obs;
-        else
-            obsLP = smooth(obs,3); %30min low pass 
-        end
-       
-        %remove the drying effects
-        mask=diff(obsLP);
-        mask=[0;mask];
-        mask=abs(mask)>0.005;
-        mask = double(mask);
-        mask(mask==0) = NaN; % or A(~A)=NaN
-        obsLP=mask.*obsLP;
+        obsLP=obs;
+        % example of water level filter
+        %         obsLP = smooth(obs,9);
+        %
+        %         mask=diff(obsLP);
+        %         mask=[0;mask];
+        %         mask=abs(mask)>0.005;
+        %         mask = double(mask);
+        %         mask(mask==0) = NaN; % or A(~A)=NaN
+        %         obsLP=mask.*obsLP;
         
         if isempty(nanmax(obs))==1
             continue %no obs.
         end
+        
         %model data
         mod = data{3};
         mod(mod<-1000) = NaN; %dry value
-        mod(mod>5.0) = NaN; %non-physical value
+        mod(mod>5) = NaN; %non-phyiscal value
         metacorr=corrcoef([obsLP,mod],'rows','complete');
         R(jj,1)=metacorr(2);
         
-        landfall = datenum([2012,10,29,23,00,00]); %landfall time
+        landfall = datenum([2016,10,8,00,00,00]); %landfall time
+        
         if plotFig == 1
+            %subplot(2,1,1)
+            KeptNames{jj,:}=['MATTHEW_V2',num2str(NOAAStaExist{i,1})];
             
             hObs=plot(datetime(1:length(obsLP)),obsLP,'bo'); hold on;
-            hMod=plot(datetime(1:length(mod)),mod+OFFSET,'r','linewi',3);
+            OFFSET=nanmean(mod-obs); %<--mean error if you don't have the steric anomaly in your model 
+            hMod=plot(datetime(1:length(mod)),mod-OFFSET,'r','linewi',3);
             legend([hObs,hMod],'Obs.','Mod.','position','best');
             datetick('x','dd');
-            xlabel('Days in October 2012');
+            xlabel('Days in October 2016');
             ylabel('m above NAVD88');
-            ylim([-1.0 4.5]);
-            plot(linspace(landfall,landfall,100),linspace(-1.0,4.5,100),'g--','linewi',3);
-            title([StationNames{i,:}]);
+            ylim([min(obs)-0.50 max(obs)+0.50
+                ]);
+            plot(linspace(landfall,landfall,100),linspace(-1.0,4.0,100),'g--','linewi',3);
+            
+%             subplot(2,1,2)
+%             plot(locs(i,1),locs(i,2),'r.','MarkerSize',15); 
+%             plot_google_map
+        
+            title([num2str(NOAAStaExist{i,1})],'Interpreter', 'none');
             makepretty(gcf,gca,16);
-            print(['SV2',StationNames{i,:}],'-dpng','-r300');   
+            print(KeptNames{jj,:},'-dpng','-r300');
             
             obsMax(jj) = nanmax(obsLP);
             obsMean(jj) = nanmean(obsLP);
-            modMax(jj) = nanmax(mod)+OFFSET;
-            modMean(jj)= nanmean(mod);
-            %rmse 
+            modMax(jj) = nanmax(mod)-OFFSET;
+            modMean(jj)= nanmean(mod)-OFFSET;
             
+            %rmse
             %(y - yhat)    % Errors
-            %(y - yhat).^2   % Squared Error 
+            %(y - yhat).^2   % Squared Error
             %mean((y - yhat).^2)   % Mean Squared Error
             %RMSE = sqrt(mean((y - yhat).^2));  % Root Mean Squared Error
-            RMSE(jj) = sqrt(nanmean((obsLP - mod).^2));  % Root Mean Squared Error            
+            RMSE(jj) = sqrt(nanmean((obsLP - mod).^2));  % Root Mean Squared Error
             MAE(jj)=nanmean(abs(mod-obs));
-            maxEleMAE(jj)=abs(modMax(jj)-obsMax(jj));
-            KeptNames{jj,:}=StationNames{i,:};
+            maxEleMAE(jj)=nanmean(abs(modMax(jj)-obsMax(jj)))
             Kept(jj,:) = locs(i,:);
             jj=jj+1;
-            createxmlfile(KeptNames,Kept,obsMax,modMax,0.2,'ZetaGE1pt5m_amp','SandyVal');
+            createxmlfile(KeptNames,Kept,obsMax,modMax,0.2,'ZETAMAX','MATTHEW_NOAA_V02');        
             close all;
             
         else
@@ -103,44 +102,30 @@ for i = 1:noStat
             obsMean(jj) = nanmean(obsLP);
             modMax(jj) = nanmax(mod)+OFFSET;
             modMean(jj)= nanmean(mod)+OFFSET;
-            
-            %rmse 
-            
+            KeptNames{jj,:}=['MATTHEW_V2',num2str(NOAAStaExist{i,1})];
+            %rmse    
             %(y - yhat)    % Errors
-            %(y - yhat).^2   % Squared Error 
+            %(y - yhat).^2   % Squared Error
             %mean((y - yhat).^2)   % Mean Squared Error
             %RMSE = sqrt(mean((y - yhat).^2));  % Root Mean Squared Error
-            RMSE(jj) = sqrt(nanmean((obsLP - mod).^2));  % Root Mean Squared Error            
+            RMSE(jj) = sqrt(nanmean((obsLP - mod).^2));  % Root Mean Squared Error
             MAE(jj)=nanmean(abs(mod-obs));
-            maxEleMAE(jj)=abs(modMax(jj)-obsMax(jj))
-            KeptNames{jj,:}=StationNames{i,:};
+            maxEleMAE(jj)=nanmean(abs(modMax(jj)-obsMax(jj)));
             Kept(jj,:) = locs(i,:);
             jj=jj+1;
-            createxmlfile(KeptNames,Kept,obsMax,modMax,0.2,'ZetaGE1pt5m_amp','SandyVal');
+            createxmlfile(KeptNames,Kept,obsMax,modMax,0.2,'ZETAMAX','MATTHEW_NOAA_V02');        
         end
         fclose(fid);
     end
     i
-    
-    
-    
 end
 
 
-hist(maxEleMAE,[.05:0.1:2]); hold on; 
-plot(linspace(0.5,0.5,100),linspace(0,50,100),'g','linewi',2);
-h=text(0.5,35,'Excellent');
-set(h, 'rotation', 90)
-plot(linspace(1.0,1.0,100),linspace(0,50,100),'y','linewi',2);
-h=text(1.0,35,'Acceptable');
-set(h, 'rotation', 90)
-plot(linspace(1.5,1.5,100),linspace(0,50,100),'r','linewi',2);
-h=text(1.5,35,'Poor');
-set(h, 'rotation', 90)
-title('Hurricane Sandy, $|simulated - observed|_{\infty}$','Interpreter','LaTeX'); 
-xlabel('$|simulated - observed|_{\infty}$','Interpreter','Latex'); 
-ylabel('Number of stations','Interpreter','Latex'); 
-makepretty(gcf,gca,16); 
+hist(maxEleMAE,[.05:0.1:2]); hold on;
+plot(linspace(0.5,0.5,100),linspace(0,50,100),'r');
+title('Hurricane Matthew, Max Ele. M.A.E.');
+xlabel('M.A.E.'); ylabel('Number of stations');
+makepretty(gcf,gca,16);
 
 % figure;
 % for i = 1 : 119
